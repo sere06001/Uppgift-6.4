b58b5ef23b89c7415d539d8e691af4d78fc8facf
sere06001 <sere06001@utb.vaxjo.se>
2024-02-15 13:06:32 +0100

    Add project files.

---
+++ Uppgift 6.4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+namespace uppgift6_4
+{
+    class Program
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                Console.Write("Bas?: ");
+                double bas = double.Parse(Console.ReadLine());
+                Console.Write("Exponent?: ");
+                int exponent = int.Parse(Console.ReadLine());
+                Console.WriteLine($"{bas}^{exponent} = {Potenser(bas, exponent)}");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+        static double Potenser(double bas, int exponent)
+        {
+            double produkt = 1;
+            for (int i = 0; i < exponent; i++)
+            {
+                produkt *= bas;
+            }
+            return produkt;
+        }
+    }
+}
